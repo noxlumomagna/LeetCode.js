1eaf90ac7926b0b53d97658a48b97f27cb1e55ea
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-06-14 19:29:30 -0400

    Create Contains Duplicate.js

---
+++ Contains Duplicate.js
@@ -0,0 +1,26 @@
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//create a memory
+//iterate through array and check all elements for repeat values
+  //if value is "new" push to memory
+  //if value does exist in memory, return true
+
+function containsDuplicate(num){
+  let memory = {};
+    for(let i=0; i<num.length; i++)
+    if(memory[num[i]] == undefined){
+        memory[num[i]] = "new memory"
+    } else {
+        return true;
+    }
+    return false;
+};
+
+or 
+
+function containsDuplicate(num){
+num.sort((a,b)=>a-b);
+  for(let i=0; i<num.length; i++){
+    if (i > 0 && num[i-1] == num[i]) return true; 
+}  
+return false;
+}

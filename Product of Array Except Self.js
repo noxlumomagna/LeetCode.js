97d374ef5ec2420f48f1d15c3412aff861fa2ae6
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-07-26 17:15:00 -0400

    Update Product of Array Except Self.js

--- Product of Array Except Self.js
+++ Product of Array Except Self.js
@@ -5,16 +5,16 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
 You must write an algorithm that runs in O(n) time and without using the division operation.
 
 function productExceptSelf(nums){
-    let answer = [];
-    let product = 1;
-    for(let i=0; i<nums.length; i++){
-        answer[i] = product
-    product = nums[i] * product
-    }
-    product = 1
-    for(let j=nums.length-1; j>=0; j--){
-        answer[j] = product * answer[j]
-        product = nums[j] * product
-    }
-    return answer
+  let outputArr = []
+  let product = 1;
+  for(let i=0; i<array.length;i++){
+    outputArr[i] = product;
+    product = array[i] * product;
+  }
+  product = 1;
+  for(let j=array.length-1; j>=0; j--){
+    outputArr[j] = product * outputArr[j]
+    product = array[j] * product
+  }
+  return outputArr
 };

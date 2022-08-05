b6f463dfee9ca10a9ca2bbf253081ce40c1c721a
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-08-05 15:30:38 -0400

    Create Roman to Integer.js

---
+++ Roman to Integer.js
@@ -0,0 +1,38 @@
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+function romanToInt(s){
+    let romans = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+       let result = 0
+        for (i=0; i < s.length; i++){
+        let current = romans[s[i]];
+        let next = romans[s[i+1]];
+
+        if (current < next){
+            result = result + next - current
+            i++
+        } else {
+            result = result + current
+        }
+    }
+    return result; 
+};

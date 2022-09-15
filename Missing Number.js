4fef0b4ab77136ea3f58f9aec2b6bcc22bc62301
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-15 18:26:14 -0400

    Update Missing Number.js

--- Missing Number.js
+++ Missing Number.js
@@ -1,5 +1,5 @@
 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
 function missingNumber(nums){
-    return ((1+nums.length)*nums.length)/2 - nums.reduce((a,b)=>(a+=b),0)
+    return ((1+nums.length)*nums.length)/2 - nums.reduce((a,b)=>(a+b),0)
 }

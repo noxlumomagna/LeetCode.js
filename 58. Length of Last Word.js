d71a5acd315cc19e4f562d802056f78415d85a24
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-09-26 21:31:41 -0400

    Create 58. Length of Last Word.js

---
+++ 58. Length of Last Word.js
@@ -0,0 +1,8 @@
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+function lengthOfLastWord(s){
+    s = s.trim().split(' ')
+    return s[s.length-1].length
+}
